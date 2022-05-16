b48aab6c30400a7d501721be2e6da380542d076b
srh-golubski <srh.sexton@gmail.com>
2022-05-15 20:16:30 -0500

    Finished reading

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,43 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title","All Events");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
+        return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
+
+}
